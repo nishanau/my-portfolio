4a67eeb8b6886673d58b342e58edfb70e968e00d
nishanau <nishanau83@gmail.com>
2024-06-08 14:47:09 +1000

    Updated  website 2

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Project = ({ title, description }) => {
+const Project = ({ title, description, url }) => {
   return (
     <article>
-      <h3>{title}</h3>
+      <h3><a href={url} target="_blank" rel="noopener noreferrer">{title}</a></h3>
       <p>{description}</p>
     </article>
   );
