908395ecade0966da6766e41d331144b5a68bd59
nishanau <nishanau83@gmail.com>
2024-06-10 12:20:27 +1000

    added a new project conway's game of life

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,35 @@
-import React from 'react';
-import Header from '../components/Header';
-import Project from '../components/Project';
-import Footer from '../components/Footer';
+import React from "react";
+import Header from "../components/Header";
+import Project from "../components/Project";
+import Footer from "../components/Footer";
 
 const projects = [
   {
-    title: 'UI/UX Design for Interactive App',
-    description: 'Created UI/UX design for a critical Bushfire Info & Management App, incorporating PACT (People, Activities, Context, Technologies) analysis. From low-fidelity to high-fidelity, I tailored the interface based on user personas and scenarios, ensuring an intuitive design for efficient bushfire information dissemination and management, aligning with PACT components.',
-    url: 'https://app.visily.ai/projects/4873b486-50b5-466e-b503-2eff441d80e5/boards/470322' // Replace with the actual URL
+    title: "UI/UX Design for Interactive App",
+    description:
+      "Created UI/UX design for a critical Bushfire Info & Management App, incorporating PACT (People, Activities, Context, Technologies) analysis. From low-fidelity to high-fidelity, I tailored the interface based on user personas and scenarios, ensuring an intuitive design for efficient bushfire information dissemination and management, aligning with PACT components.",
+    url: "https://app.visily.ai/projects/4873b486-50b5-466e-b503-2eff441d80e5/boards/470322", 
   },
 
   {
-    title: 'Cryptocurrency Digital Wallet Web App',
-    description: 'A digital wallet for cryptocurrencies built using React and Node JS, where users can create a new wallet, restore old wallets using seed phrases or private keys. The app also fetches data from APIs to show prices of cryptocurrency assets, to fetch wallet details and its past transactions from block explorers. Users can send and receive cryptocurrencies.',
-    url: 'http://3.25.198.253:3001/' // Replace with the actual URL
+    title: "Cryptocurrency Digital Wallet Web App",
+    description:
+      "A digital wallet for cryptocurrencies built using React and Node JS, where users can create a new wallet, restore old wallets using seed phrases or private keys. The app also fetches data from APIs to show prices of cryptocurrency assets, to fetch wallet details and its past transactions from block explorers. Users can send and receive cryptocurrencies.",
+    url: "http://3.25.198.253:3001/", 
   },
   {
-    title: 'Next.js Calculator App',
-    description: 'This is a simple calculator application built using Next.js and React, styled with Tailwind CSS. The app supports basic arithmetic operations and handles input errors gracefully. The app has been configured to be statically exported and deployed to GitHub Pages, ensuring efficient and reliable access.',
-    url: 'http://3.25.198.253:3001/' // Replace with the actual URL
+    title: "Next.js Calculator App",
+    description:
+      "This is a simple calculator application built using Next.js and React, styled with Tailwind CSS. The app supports basic arithmetic operations and handles input errors gracefully. The app has been configured to be statically exported and deployed to GitHub Pages, ensuring efficient and reliable access.",
+    url: "https://nishanau.github.io/nextjs-calculator/", 
   },
 
-
+  {
+    title: "Conway's Game of Life",
+    description:
+      "A React implementation of Conway's Game of Life, a cellular automaton devised by mathematician John Conway. This interactive app allows users to start, stop, and clear the simulation, as well as adjust the grid size. The game demonstrates the evolution of cell patterns based on initial states and simple rules.",
+    url: "https://nishanau.github.io/conway-s-game-of-life", 
+  },
 ];
 
 const Home = () => {
@@ -32,7 +40,12 @@ const Home = () => {
         <section id="projects">
           <h2>My Projects</h2>
           {projects.map((project, index) => (
-            <Project key={index} title={project.title} description={project.description} url={project.url} />
+            <Project
+              key={index}
+              title={project.title}
+              description={project.description}
+              url={project.url}
+            />
           ))}
         </section>
       </main>
