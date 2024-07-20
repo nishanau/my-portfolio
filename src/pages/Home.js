ca73e6ee2afa73148f7831c13599c3a00c8d0b55
nishanau <nishanau83@gmail.com>
2024-07-21 09:43:01 +1000

    added new project NextJS Shopping App and fixed descriptions of all projects

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,35 +8,38 @@ const projects = [
     title: "Conway's Game of Life",
     description:
       "A React implementation of Conway's Game of Life, a cellular automaton devised by mathematician John Conway. This interactive app allows users to start, stop, and clear the simulation, as well as adjust the grid size. The game demonstrates the evolution of cell patterns based on initial states and simple rules.",
-    url: "https://nishanau.github.io/conway-s-game-of-life", 
+    url: "https://nishanau.github.io/conway-s-game-of-life",
+  },
+  {
+    title: "NextJS Full Stack App",
+    description:
+      "This is a full stack application built using Next.js. The app features user authentication with email, Google, and Facebook using NextAuth, and switches between dark mode and light mode based on the user's browser preferences. It includes product cards to display products and categories to filter the products. Tasks achieved include sign-up authentication, user profile management, and product display. Upcoming features include search functionality, user profile, adding products to cart and processing purchases, and adding skeletons for better UI.",
+    url: "http://http://3.27.247.61:3000/", 
   },
   {
     title: "Computer Networks Mapper",
     description:
-      "Developed a web-based application that optimizes and visually maps computer networks based on user input. The tool calculates efficient subnet allocations and necessary IP addresses for routers and switches. It features a responsive UI, network visualization using vis-network, and export options for diagrams. The frontend is built with React and hosted on GitHub Pages, while the backend runs on Node.js and Python on an AWS EC2 instance. This project showcases skills in network design, web development, and cloud deployment.",
-    url: "https://nishanau.github.io/ComputerNetworksSolution/", 
+      "Developed a web-based application that optimizes and visually maps computer networks based on user input. The tool calculates efficient subnet allocations and necessary IP addresses for routers and switches. It features a responsive UI, network visualization using vis-network, and export options for diagrams. The frontend is built with React and hosted on GitHub Pages, while the backend runs on Node.js and Python on an AWS EC2 instance. This project showcases skills in network design, web development, and cloud deployment.",
+    url: "https://nishanau.github.io/ComputerNetworksSolution/",
   },
   {
     title: "UI/UX Design for Interactive App",
     description:
-      "Created UI/UX design for a critical Bushfire Info & Management App, incorporating PACT (People, Activities, Context, Technologies) analysis. From low-fidelity to high-fidelity, I tailored the interface based on user personas and scenarios, ensuring an intuitive design for efficient bushfire information dissemination and management, aligning with PACT components.",
-    url: "https://app.visily.ai/projects/4873b486-50b5-466e-b503-2eff441d80e5/boards/470322", 
+      "Created UI/UX design for a critical Bushfire Info & Management App, incorporating PACT (People, Activities, Context, Technologies) analysis. From low-fidelity to high-fidelity, I tailored the interface based on user personas and scenarios, ensuring an intuitive design for efficient bushfire information dissemination and management, aligning with PACT components. This project enhanced my skills in UI/UX design, user research, and prototyping.",
+    url: "https://app.visily.ai/projects/4873b486-50b5-466e-b503-2eff441d80e5/boards/470322",
   },
-
   {
     title: "Cryptocurrency Digital Wallet Web App",
     description:
-      "A digital wallet for cryptocurrencies built using React and Node JS, where users can create a new wallet, restore old wallets using seed phrases or private keys. The app also fetches data from APIs to show prices of cryptocurrency assets, to fetch wallet details and its past transactions from block explorers. Users can send and receive cryptocurrencies.",
-    url: "http://3.25.186.18:3001/", 
+      "A digital wallet for cryptocurrencies built using React and Node.js, where users can create a new wallet, restore old wallets using seed phrases or private keys. The app fetches data from APIs to show prices of cryptocurrency assets, fetch wallet details, and view past transactions from block explorers. Users can send and receive cryptocurrencies. This project improved my skills in API integration, state management, and security in web development.",
+    url: "http://3.25.186.18:3001/",
   },
   {
     title: "Next.js Calculator App",
     description:
-      "This is a simple calculator application built using Next.js and React, styled with Tailwind CSS. The app supports basic arithmetic operations and handles input errors gracefully. The app has been configured to be statically exported and deployed to GitHub Pages, ensuring efficient and reliable access.",
-    url: "https://nishanau.github.io/nextjs-calculator/", 
+      "This is a simple calculator application built using Next.js and React, styled with Tailwind CSS. The app supports basic arithmetic operations and handles input errors gracefully. The app has been configured to be statically exported and deployed to GitHub Pages, ensuring efficient and reliable access. This project helped me enhance my skills in Next.js, React, and responsive design.",
+    url: "https://nishanau.github.io/nextjs-calculator/",
   },
-
-  
 ];
 
 const Home = () => {
